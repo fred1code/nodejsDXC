6a72adbf4bb0af9e7dd42ca48c76f89dfbf78485
alfredo robles <alfredoroblesing@gmail.com>
2019-11-06 10:01:10 -0600

    jest finish

--- __test__/index.test.js
+++ __test__/index.test.js
@@ -1,5 +1,4 @@
 const request = require('supertest');
-
 const app = require('../index');
 
 describe('prueba server metodo get', () => {
@@ -10,7 +9,6 @@ describe('prueba server metodo get', () => {
         });
     });
 });
-
 
 describe('prueba server metodo get parametros mal', () => {
     test('responder a el metodo GET', done => {
@@ -24,6 +22,36 @@ describe('prueba server metodo get parametros mal', () => {
 describe('prueba server metodo get con error', () => {
     test('responder a el metodo GET', done => {
         request(app).get('/algo').then((response) => {
+            expect(response.statusCode).toBe(500);
+            done();
+        });
+    });
+});
+
+
+describe('prueba server metodo POST 200', () => {
+    test('responder a el metodo post', done => {
+        request(app).post('/test').type('text').send('[5,6,3,4,1]').then((response) => {
+            expect(response.statusCode).toBe(200);
+            done();
+        });
+    });
+});
+
+
+describe('prueba server metodo POST 422', () => {
+    test('responder a el metodo post', done => {
+        request(app).post('/test').type('text').send('[5,6,3,4,1').then((response) => {
+            expect(response.statusCode).toBe(422);
+            done();
+        });
+    });
+});
+
+
+describe('prueba server metodo POST error 500', () => {
+    test('responder a el metodo post', done => {
+        request(app).post('/algo').type('text').send('[5,6,3,4,1]').then((response) => {
             expect(response.statusCode).toBe(500);
             done();
         });
